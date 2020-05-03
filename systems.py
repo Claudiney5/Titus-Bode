4584713def83f4ed20b8dc31f9c10c779cc9b21c
Claudiney Martins <58961647+Claudiney5@users.noreply.github.com>
2020-05-03 12:13:16 -0300

    finished systems
    need build a function for simulate systems (tb_law.py)

--- systems.py
+++ systems.py
@@ -1,8 +1,11 @@
+import tb_law
 from csv import DictReader
 
-with open('compositepars.csv') as data:
-    reader_plan = DictReader(data)  # cada linha uma lista
 
+with open('compositepars.csv') as data:
+    reader_plan = DictReader(data)  # for each line a dict of a planet
+
+    # cleaning data ------------------------------------------------
     system = {}  # dict of dicts
     list_sys = []
     i = 0
@@ -11,31 +14,21 @@ with open('compositepars.csv') as data:
         system[i] = {'name': line['fpl_hostname'], 'planet ' + line['fpl_letter']: line['fpl_letter'], 'dist ' + line['fpl_letter']: line['fpl_smax']}
         i = i + 1
 
+    # list of systems name (only one for each system)---------------
     list_sys = list(set(list_sys))
     list_sys.sort()
 
+    # creating a planet list --------------------------------------
+    planets = list(system.values())
+
+    # creating a dict with systems assembled -----------------------------
+    j = 0
     sys_work = {}
-    i = 0
     for name in list_sys:
-        sys_work[name] = {'name': name}
+        sys_work[name] = {}
 
-    for line in system:
-        for name in list_sys:
-            if name == system[line]['name']:
-                sys_work[name] = sys_work[name].update(system[line])
-
-        print(sys_work)
-
-
-''' for n in range(0, i):
-        try:
-            system[n]['planet b'] == 'planet b'
-            for m in range(n + 1, i):
-                if system[n]['name'] == system[m]['name']:
-                    print(system[n].update(system[m]))
-        except:
-            pass
-'''
-
-
-
+        for j in range(0, i):
+            if name == planets[j]['name']:
+                sys_work[name].update(planets[j])
+    print(sys_work)
+    print(system)
---
+++ tb_law.py
@@ -0,0 +1,11 @@
+"""
+This file use the approach for Titius-Bode Law from the paper:
+
+Self-Organizing Systems in Planetary Physics : Harmonic Resonances of Planet
+and Moon Orbits - Markus J. Aschwanden
+(Lockheed Martin, Solar and Astrophysics Laboratory, Org. A021S, Bldg. 252, 3251 Hanover St., Palo
+Alto, CA 94304, USA; e-mail: aschwanden@lmsal.com)
+
+In conclusion he say: "The results may be useful for searches of exo-planets orbiting around stars."
+
+"""
